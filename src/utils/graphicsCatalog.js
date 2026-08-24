b256e5c4b6467f12b53d18118cc5a6187974e681
Darkmintis <darkmintis@gmail.com>
2026-08-24 16:13:06 +0545

    feat: add template graphics catalog

---
+++ src/utils/graphicsCatalog.js
@@ -0,0 +1,19 @@
+/** Decorative SVGs in /public/graphics - recolor via #00A / #00B / #00C slots. */
+export const GRAPHICS = [
+  { id: 'blob-cluster', src: 'blob-cluster.svg', label: 'Blobs' },
+  { id: 'wave-layers', src: 'wave-layers.svg', label: 'Waves' },
+  { id: 'rings', src: 'rings.svg', label: 'Rings' },
+  { id: 'orb-cluster', src: 'orb-cluster.svg', label: 'Orbs' },
+  { id: 'sparkles', src: 'sparkles.svg', label: 'Sparkles' },
+  { id: 'confetti', src: 'confetti.svg', label: 'Confetti' },
+  { id: 'arch', src: 'arch.svg', label: 'Arch' },
+  { id: 'dots-grid', src: 'dots-grid.svg', label: 'Dots' },
+  { id: 'leaf-flourish', src: 'leaf-flourish.svg', label: 'Flourish' },
+  { id: 'diagonal-bars', src: 'diagonal-bars.svg', label: 'Bars' },
+];
+
+export const SLOT_COLORS = {
+  a: '#00A',
+  b: '#00B',
+  c: '#00C',
+};
